e3dd29e0e2b5bcf327e8679deecc2f3abeeeabc9
Arun Sivakumar <arunsivakumar@live.com>
2020-04-26 18:28:46 +0800

    add ex-2a

---
+++ 02_introduction/code/exercise_2a.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Hello World")
+	fmt.Println("Hi! My name is %s. I have lived in %s for %d years. They say the weather is amazing, which is %t.", "Arun S", "Cbe", 25, true)
+}
